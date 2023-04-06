7b4e59d9ca94b87f2bc2bc4e1796cba4f66650d2
Ostabo <osi.borkenhagen@gmail.com>
2023-04-06 16:04:10 +0200

    fix gui di

--- gui/src/main/scala/service/GuiService.scala
+++ gui/src/main/scala/service/GuiService.scala
@@ -1,8 +1,8 @@
 package service
 
+import di.CoreModule.given_ControllerInterface_Player
 import lib.GUI
 import scalafx.application.JFXApp3
-import service.HexModule.given_ControllerInterface_Player
 
 object GuiService extends JFXApp3:
   override def start(): Unit =
