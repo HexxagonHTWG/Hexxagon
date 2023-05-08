d6902feb4d00801fd272ee696c2d22798bad0c93
Ostabo <osi.borkenhagen@gmail.com>
2023-05-08 18:08:38 +0200

    #34 - use logger + implement retry loop for db connection

--- persistence/src/main/scala/lib/database/slick/DAOSlick.scala
+++ persistence/src/main/scala/lib/database/slick/DAOSlick.scala
@@ -1,6 +1,7 @@
 package lib.database.slick
 
 import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.StrictLogging
 import di.{FlexibleProviderModule, PersistenceModule}
 import lib.database.DAOInterface
 import lib.database.slick.tables.{FieldTable, GameTable}
@@ -16,9 +17,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.{Duration, DurationInt}
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
+import scala.util.control.Breaks.{break, breakable}
 import scala.util.{Failure, Success, Try}
 
-object DAOSlick extends DAOInterface[Player]:
+object DAOSlick extends DAOInterface[Player] with StrictLogging:
 
   private lazy val config = ConfigFactory.load()
   private val databaseUrl: String =
@@ -27,7 +29,7 @@ object DAOSlick extends DAOInterface[Player]:
       s"${config.getString("db.port")}/" +
       s"${config.getString("db.name")}?serverTimezone=CET"
 
-  println(databaseUrl)
+  logger.debug(s"Database URL: $databaseUrl")
   private val database = Database.forURL(
     url = databaseUrl,
     driver = config.getString("db.driver"),
@@ -35,7 +37,8 @@ object DAOSlick extends DAOInterface[Player]:
     password = config.getString("db.password")
   )
 
-  private val maxWaitTime: Duration = 5 seconds
+  private val maxWaitSeconds: Duration = config.getInt("db.maxWaitSeconds") seconds
+  private val connectionRetryAttempts = config.getInt("db.connectionRetryAttempts")
 
   private val gameTable = new TableQuery(new GameTable(_))
   private val fieldTable = new TableQuery(new FieldTable(_))
@@ -43,13 +46,13 @@ object DAOSlick extends DAOInterface[Player]:
   private val setup: DBIOAction[Unit, NoStream, Effect.Schema] =
     DBIO.seq(gameTable.schema.createIfNotExists, fieldTable.schema.createIfNotExists)
 
-  try {
-    Await.result(database.run(setup), maxWaitTime)
-  } catch {
-    case _: SQLNonTransientException =>
-      println("Waiting for DB connection")
-      Thread.sleep(5000)
-      Await.result(database.run(setup), maxWaitTime)
+  breakable {
+    for (i <- 1 to connectionRetryAttempts)
+      Try(Await.result(database.run(setup), maxWaitSeconds)) match
+        case Success(_) => logger.info("DB connection established"); break
+        case Failure(_) =>
+          logger.info(s"Waiting for DB connection, attempt $i/$connectionRetryAttempts")
+          Thread.sleep(maxWaitSeconds.toMillis)
   }
 
   override def save(field: FieldInterface[Player]): Try[Unit] =
@@ -57,7 +60,7 @@ object DAOSlick extends DAOInterface[Player]:
       val insertAction = gameTable returning gameTable.map(_.id)
         += (0, field.matrix.row, field.matrix.col)
 
-      val gameId = Await.result(database.run(insertAction), maxWaitTime)
+      val gameId = Await.result(database.run(insertAction), maxWaitSeconds)
 
       insertField(gameId, field)
     }
@@ -69,7 +72,7 @@ object DAOSlick extends DAOInterface[Player]:
           val cell = field.matrix.cell(col, row)
           val insertAction = fieldTable += (0, gameId, row, col, cell.toString)
 
-          Await.result(database.run(insertAction), maxWaitTime)
+          Await.result(database.run(insertAction), maxWaitSeconds)
         }
       }
     }
@@ -82,12 +85,12 @@ object DAOSlick extends DAOInterface[Player]:
       val fieldAction = gameId.map(id => fieldTable.filter(_.gameId === id))
         .getOrElse(fieldTable.filter(_.gameId === maxGameId))
 
-      val gameResult = Await.result(database.run(gameAction.result), maxWaitTime)
+      val gameResult = Await.result(database.run(gameAction.result), maxWaitSeconds)
       val rows = gameResult.head._2
       val cols = gameResult.head._3
       var hexField = FlexibleProviderModule(rows, cols).given_FieldInterface_Player
 
-      val fieldResult = Await.result(database.run(fieldAction.result), maxWaitTime)
+      val fieldResult = Await.result(database.run(fieldAction.result), maxWaitSeconds)
       for (row <- 0 until rows) {
         for (col <- 0 until cols) {
           val cell = fieldResult.filter(_._3 == row).filter(_._4 == col).head._5
@@ -102,8 +105,8 @@ object DAOSlick extends DAOInterface[Player]:
       val gameAction = gameTable.filter(_.id === gameId).update((gameId, field.matrix.row, field.matrix.col))
       val fieldAction = fieldTable.filter(_.gameId === gameId).delete
 
-      Await.result(database.run(gameAction), maxWaitTime)
-      Await.result(database.run(fieldAction), maxWaitTime)
+      Await.result(database.run(gameAction), maxWaitSeconds)
+      Await.result(database.run(fieldAction), maxWaitSeconds)
 
       insertField(gameId, field)
     }
@@ -116,7 +119,7 @@ object DAOSlick extends DAOInterface[Player]:
       val fieldAction = gameId.map(id => fieldTable.filter(_.gameId === id).delete)
         .getOrElse(fieldTable.filter(_.gameId === maxGameId).delete)
 
-      Await.result(database.run(gameAction), maxWaitTime)
-      Await.result(database.run(fieldAction), maxWaitTime)
+      Await.result(database.run(gameAction), maxWaitSeconds)
+      Await.result(database.run(fieldAction), maxWaitSeconds)
       Success(())
     }
