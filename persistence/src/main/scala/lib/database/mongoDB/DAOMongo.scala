0e5944194206f5f6083c4d444c382f6a3ebe86b7
Ostabo <osi.borkenhagen@gmail.com>
2023-05-15 16:28:33 +0200

    #40 - use config to switch between db implementations + switch docker db impl instead of new service

--- persistence/src/main/scala/lib/database/mongoDB/DAOMongo.scala
+++ persistence/src/main/scala/lib/database/mongoDB/DAOMongo.scala
@@ -9,9 +9,9 @@ import lib.field.FieldInterface
 import lib.json.HexJson
 import org.mongodb.scala.*
 import org.mongodb.scala.bson.ObjectId
-import org.mongodb.scala.model.Updates.set
 import org.mongodb.scala.model.Filters.equal
 import org.mongodb.scala.model.Projections.excludeId
+import org.mongodb.scala.model.Updates.set
 import org.mongodb.scala.result.{DeleteResult, InsertOneResult, UpdateResult}
 import play.api.libs.json.JsObject
 
@@ -25,15 +25,15 @@ object DAOMongo extends DAOInterface[Player] with StrictLogging {
   private lazy val config = ConfigFactory.load()
   private val databaseUrl: String =
     s"mongodb://" +
-      s"${config.getString("db.mongodb.user")}:" +
-      s"${config.getString("db.mongodb.password")}@" +
-      s"${config.getString("db.mongodb.host")}:" +
-      s"${config.getString("db.mongodb.port")}" +
+      s"${config.getString("db.user")}:" +
+      s"${config.getString("db.password")}@" +
+      s"${config.getString("db.host")}:" +
+      s"${config.getString("db.port")}" +
       "/?authSource=admin"
 
   logger.debug(s"Database URL: $databaseUrl")
   private val client: MongoClient = MongoClient(databaseUrl)
-  private val db: MongoDatabase = client.getDatabase("hexxagon")
+  private val db: MongoDatabase = client.getDatabase(config.getString("db.name"))
   private val gameCollection: MongoCollection[Document] = db.getCollection("game")
 
   private val maxWaitSeconds: Duration = config.getInt("db.maxWaitSeconds") seconds
@@ -45,17 +45,6 @@ object DAOMongo extends DAOInterface[Player] with StrictLogging {
       val currentGameId = gameIdCounter % maxGameCount
       update(currentGameId, field)
       gameIdCounter += 1
-    }
-
-  override def load(gameId: Option[Int]): Try[FieldInterface[Player]] =
-    Try {
-      val searchId = gameId.getOrElse((gameIdCounter match {
-        case 0 => 0
-        case _ => gameIdCounter - 1
-      }) % maxGameCount)
-      val document = Await.result(gameCollection.find(equal("_id", searchId)).projection(excludeId())
-        .first().head(), maxWaitSeconds)
-      HexJson.decode(document.get("game").get.asString().getValue).get
     }
 
   override def update(gameId: Int, field: FieldInterface[Player]): Try[Unit] =
@@ -72,6 +61,17 @@ object DAOMongo extends DAOInterface[Player] with StrictLogging {
         })
     }
 
+  override def load(gameId: Option[Int]): Try[FieldInterface[Player]] =
+    Try {
+      val searchId = gameId.getOrElse((gameIdCounter match {
+        case 0 => 0
+        case _ => gameIdCounter - 1
+      }) % maxGameCount)
+      val document = Await.result(gameCollection.find(equal("_id", searchId)).projection(excludeId())
+        .first().head(), maxWaitSeconds)
+      HexJson.decode(document.get("game").get.asString().getValue).get
+    }
+
   override def delete(gameId: Option[Int]): Try[Unit] =
     Try {
       val deleteId = gameId.getOrElse({
@@ -81,11 +81,11 @@ object DAOMongo extends DAOInterface[Player] with StrictLogging {
 
       gameCollection.deleteOne(equal("_id", deleteId))
         .subscribe(new Observer[DeleteResult] {
-        override def onNext(result: DeleteResult): Unit = logger.debug(s"Deleted: $result with id $deleteId")
+          override def onNext(result: DeleteResult): Unit = logger.debug(s"Deleted: $result with id $deleteId")
 
-        override def onError(e: Throwable): Unit = logger.error(s"Failed: $e")
+          override def onError(e: Throwable): Unit = logger.error(s"Failed: $e")
 
-        override def onComplete(): Unit = logger.debug("Completed")
-      })
+          override def onComplete(): Unit = logger.debug("Completed")
+        })
     }
 }
